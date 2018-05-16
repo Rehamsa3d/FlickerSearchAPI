fca9758e78d7cb74945355cdd228686b3d8b3802
ahmedxiio <ahmedxiio@raoufs-macbook-pro.local>
2018-05-16 15:20:49 +0200

    Alert message Edits

--- helper/MessageAlert.swift
+++ helper/MessageAlert.swift
@@ -9,23 +9,16 @@
 import Foundation
 import UIKit
 extension UIViewController {
-    func displayAlertMessage(messageToDisplay: String){
-        let alertController = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
-            // Code in this block will trigger when OK button tapped.
-            print("Ok button tapped");
-            //    messageToDisplay(messageToDisplay: "Email address is not valid")
-        }
-        alertController.addAction(OKAction)
-        self.present(alertController, animated: true, completion:nil)
-    }
+    
     func messageToDisplay(messageToDisplay: String){
         let alertController = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: .alert)
         let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
             // Code in this block will trigger when OK button tapped.
             print("Ok button tapped");
         }
+        
         alertController.addAction(OKAction)
         self.present(alertController, animated: true, completion:nil)
     }
+    
 }
